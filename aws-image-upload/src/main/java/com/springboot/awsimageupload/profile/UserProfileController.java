7074677812cf78daa79e9381107d9ce0f75fd83f
haomingjue <haomingjue@outlook.com>
2022-05-30 18:35:18 -0400

    Yesterday uploaded wrong folder omg

--- aws-image-upload/src/main/java/com/springboot/awsimageupload/profile/UserProfileController.java
+++ aws-image-upload/src/main/java/com/springboot/awsimageupload/profile/UserProfileController.java
@@ -40,33 +40,5 @@ public class UserProfileController {
     public byte[] downloadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId) {
         return userProfileService.downloadUserProfileImage(userProfileId);
     }
-//    private final UserProfileService userProfileService;
-//
-//
-//    @Autowired
-//    public UserProfileController(UserProfileService userProfileService) {
-//        this.userProfileService = userProfileService;
-//    }
-//
-//
-//    @GetMapping
-//    public List<UserProfile> getUserProfiles() {
-//        return userProfileService.getUserProfiles();
-//    }
-//
-//    @PostMapping(
-//            path = "{userProfileId}/image/upload",
-//            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-//            produces = MediaType.APPLICATION_JSON_VALUE
-//    )
-//    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
-//                                       @RequestParam("file") MultipartFile file) {
-//        userProfileService.uploadUserProfileImage(userProfileId, file);
-//    }
-//
-//    @GetMapping("{userProfileId}/image/download")
-//    public byte[] downloadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
-//                                           @RequestParam("file") MultipartFile file){
-//        return userProfileService.downloadUserProfileImage(userProfileId);
-//    }
+
 }
